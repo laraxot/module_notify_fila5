abf5cba1b3ea89ded5192af63d7a486e7705e987
marco76tv <marco.sottana@gmail.com>
2026-03-30 15:12:52 +0200

    .

--- laravel/Themes/Sixteen/resources/design-comuni/pages/argomenti.blade.php
+++ laravel/Themes/Sixteen/resources/design-comuni/pages/argomenti.blade.php
@@ -3,8 +3,10 @@
     Source: https://italia.github.io/design-comuni-pagine-statiche/sito/argomenti.html
     Category: Generali
     
-    NOTE: Header e Footer sono sezioni richiamate con <x-section slug="header" />
-    Componenti si registrano con namespace pub_theme::
+    NOTE:
+    - Header e Footer sono sezioni richiamate con <x-section slug="header" />
+    - Componenti si registrano con namespace pub_theme::
+    - CSS: Tailwind CSS 4 (design-comuni.css) - NO Bootstrap
 --}}
 
 @extends('pub_theme::layouts.app')
@@ -85,11 +87,7 @@
 
 @endsection
 
-@push('scripts')
-<script>
-    // Inizializza eventuali componenti interattivi
-    document.addEventListener('DOMContentLoaded', function() {
-        // Bootstrap Italia components initialization
-    });
-</script>
-@endpush
+{{-- 
+NOTE: Il CSS Tailwind è già incluso in app.css
+Non serve aggiungere @vite per CSS separati
+--}}
