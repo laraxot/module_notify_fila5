db73df0e98383add8741ef74ebed0eb2bf5c8b27
GitHub Action <action@github.com>
2026-03-30 13:13:57 +0000

    chore: sync remote subtrees [skip ci]

--- laravel/Themes/Sixteen/resources/js/app.js
+++ laravel/Themes/Sixteen/resources/js/app.js
@@ -1,11 +1,12 @@
-import Alpine from './alpine';
-import './flowbite.js';
-import './swiper.js';
-import './custom.js';
-import './design-comuni.js';
-import './filament-4x.js';
-import './agid-enforcer.js';
-import './pwa.js';
-import './performance.js';
-
-Alpine.start();
+import "./flowbite.js";
+import "./swiper.js";
+//import "./cookie-consent.js";
+import "./custom.js";
+import "./filament-4x.js";
+// Bootstrap Italia JS bundle (for header/nav components, dropdowns, etc.)
+import "bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js";
+import "./agid-enforcer.js";
+// PWA Manager
+import "./pwa.js";
+// Performance Manager
+import "./performance.js";
