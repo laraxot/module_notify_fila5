39340428e09bbc89550d2f14c3d5fac36aec923d
Marco Xot <marco76tv@hotmail.com>
2026-08-06 19:28:20 +0200

    .

--- app/Actions/SMS/SendGammuSMSAction.php
+++ app/Actions/SMS/SendGammuSMSAction.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Actions\SMS;
 
+<<<<<<< HEAD
+=======
+use function Safe\file_put_contents;
+use function Safe\tempnam;
+use function Safe\unlink;
+>>>>>>> b05b65f05 (Refactor NotifyThemeableBusinessLogicTest to simplify factory usage and improve readability)
 use Exception;
 use Illuminate\Support\Str;
 use Modules\Notify\Contracts\SMS\SmsActionContract;
@@ -13,9 +19,12 @@ use Override;
 use Spatie\QueueableAction\QueueableAction;
 use Symfony\Component\Process\Process;
 
+<<<<<<< HEAD
 use function Safe\file_put_contents;
 use function Safe\tempnam;
 use function Safe\unlink;
+=======
+>>>>>>> b05b65f05 (Refactor NotifyThemeableBusinessLogicTest to simplify factory usage and improve readability)
 
 final class SendGammuSMSAction implements SmsActionContract
 {
